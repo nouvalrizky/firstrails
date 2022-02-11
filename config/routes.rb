630b05fc53df74f055b6b35c5897e3455d138889
Nouval <nouvalr@gmail.com>
2022-02-11 14:44:57 +0800

    Add User Auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
 
   get "about", to: "about#index", as: :about
+  
+  get "sign_up", to: "registration#new"
+  post "sign_up", to: "registration#create"
 
+  get "login", to: "sessions#new"
+  post "login", to: "sessions#create"
+  delete 'logout', to: 'sessions#destroy'
+  
   root to: "main#index"
-
 end
