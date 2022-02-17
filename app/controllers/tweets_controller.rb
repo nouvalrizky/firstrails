4c36441d0209b5d8f8e77a16f093fa20cf3f8dcc
Nouval <nouvalr@gmail.com>
2022-02-18 01:54:45 +0800

    finish CRUD for tweets

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,6 @@
 class TweetsController < ApplicationController
     before_action :require_user_logged_in!
+    before_action :set_tweet, only: [:edit, :update, :destroy]
 
     def index
         @tweets = Current.user.tweets
@@ -20,10 +21,31 @@ class TweetsController < ApplicationController
         end
     end
 
+    def edit
+        
+    end
+
+    def update
+        if @tweet.update(tweet_params)
+            redirect_to tweets_path, notice: "Tweet was updated successfully!"
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+        @tweet.destroy
+        redirect_to tweets_path, alert: "Tweet was deleted successfully!"
+    end
+
     private
 
     def tweet_params
         Rails.logger.info params
         params.require(:tweet).permit(:twitter_account_id, :body, :publish_at)
+    end
+
+    def set_tweet
+        @tweet = Current.user.tweets.find(params[:id])
     end
 end
